00c37f6d5d6d2f9d570dbe119f3ccd700dde6e19
ricardo <ricardo.aguirre.99@gmail.com>
2016-05-21 03:03:11 -0300

    avance c#

---
+++ MercadoEnvio/WindowsFormsApplication1/frm_Logeo.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frm_Logeo
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lab_username = new System.Windows.Forms.Label();
+            this.lab_password = new System.Windows.Forms.Label();
+            this.tex_username = new System.Windows.Forms.TextBox();
+            this.tex_password = new System.Windows.Forms.TextBox();
+            this.but_Ingresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lab_username
+            // 
+            this.lab_username.AutoSize = true;
+            this.lab_username.Location = new System.Drawing.Point(32, 35);
+            this.lab_username.Name = "lab_username";
+            this.lab_username.Size = new System.Drawing.Size(55, 13);
+            this.lab_username.TabIndex = 0;
+            this.lab_username.Text = "Username";
+            this.lab_username.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lab_password
+            // 
+            this.lab_password.AutoSize = true;
+            this.lab_password.Location = new System.Drawing.Point(35, 103);
+            this.lab_password.Name = "lab_password";
+            this.lab_password.Size = new System.Drawing.Size(53, 13);
+            this.lab_password.TabIndex = 1;
+            this.lab_password.Text = "Password";
+            // 
+            // tex_username
+            // 
+            this.tex_username.Location = new System.Drawing.Point(116, 35);
+            this.tex_username.Name = "tex_username";
+            this.tex_username.Size = new System.Drawing.Size(121, 20);
+            this.tex_username.TabIndex = 2;
+            // 
+            // tex_password
+            // 
+            this.tex_password.Location = new System.Drawing.Point(116, 95);
+            this.tex_password.Name = "tex_password";
+            this.tex_password.Size = new System.Drawing.Size(121, 20);
+            this.tex_password.TabIndex = 3;
+            // 
+            // but_Ingresar
+            // 
+            this.but_Ingresar.Location = new System.Drawing.Point(88, 159);
+            this.but_Ingresar.Name = "but_Ingresar";
+            this.but_Ingresar.Size = new System.Drawing.Size(82, 39);
+            this.but_Ingresar.TabIndex = 4;
+            this.but_Ingresar.Text = "Ingresar";
+            this.but_Ingresar.UseVisualStyleBackColor = true;
+            // 
+            // form_Logeo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 218);
+            this.Controls.Add(this.but_Ingresar);
+            this.Controls.Add(this.tex_password);
+            this.Controls.Add(this.tex_username);
+            this.Controls.Add(this.lab_password);
+            this.Controls.Add(this.lab_username);
+            this.Name = "form_Logeo";
+            this.Text = "Logeo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lab_username;
+        private System.Windows.Forms.Label lab_password;
+        private System.Windows.Forms.TextBox tex_username;
+        private System.Windows.Forms.TextBox tex_password;
+        private System.Windows.Forms.Button but_Ingresar;
+    }
+}
+
---
+++ MercadoEnvio/WindowsFormsApplication1/frm_Tareas_Administrador.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frm_Tareas_Administrador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.but_Gestionar_Rol = new System.Windows.Forms.Button();
+            this.but_Gestionar_Rubro = new System.Windows.Forms.Button();
+            this.but_Gestionar_Usuarios = new System.Windows.Forms.Button();
+            this.but_Gestionar_Visibilidad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // but_Gestionar_Rol
+            // 
+            this.but_Gestionar_Rol.Location = new System.Drawing.Point(25, 25);
+            this.but_Gestionar_Rol.Name = "but_Gestionar_Rol";
+            this.but_Gestionar_Rol.Size = new System.Drawing.Size(121, 23);
+            this.but_Gestionar_Rol.TabIndex = 0;
+            this.but_Gestionar_Rol.Text = "Gestionar Rol";
+            this.but_Gestionar_Rol.UseVisualStyleBackColor = true;
+            // 
+            // but_Gestionar_Rubro
+            // 
+            this.but_Gestionar_Rubro.Location = new System.Drawing.Point(25, 78);
+            this.but_Gestionar_Rubro.Name = "but_Gestionar_Rubro";
+            this.but_Gestionar_Rubro.Size = new System.Drawing.Size(121, 23);
+            this.but_Gestionar_Rubro.TabIndex = 1;
+            this.but_Gestionar_Rubro.Text = "Gestionar Rubro";
+            this.but_Gestionar_Rubro.UseVisualStyleBackColor = true;
+            // 
+            // but_Gestionar_Usuarios
+            // 
+            this.but_Gestionar_Usuarios.Location = new System.Drawing.Point(25, 134);
+            this.but_Gestionar_Usuarios.Name = "but_Gestionar_Usuarios";
+            this.but_Gestionar_Usuarios.Size = new System.Drawing.Size(121, 23);
+            this.but_Gestionar_Usuarios.TabIndex = 2;
+            this.but_Gestionar_Usuarios.Text = "Gestionar Usuarios";
+            this.but_Gestionar_Usuarios.UseVisualStyleBackColor = true;
+            // 
+            // but_Gestionar_Visibilidad
+            // 
+            this.but_Gestionar_Visibilidad.Location = new System.Drawing.Point(25, 182);
+            this.but_Gestionar_Visibilidad.Name = "but_Gestionar_Visibilidad";
+            this.but_Gestionar_Visibilidad.Size = new System.Drawing.Size(121, 23);
+            this.but_Gestionar_Visibilidad.TabIndex = 3;
+            this.but_Gestionar_Visibilidad.Text = "Gestionar Visibilidad";
+            this.but_Gestionar_Visibilidad.UseVisualStyleBackColor = true;
+            // 
+            // frm_Tareas_Administrador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 249);
+            this.Controls.Add(this.but_Gestionar_Visibilidad);
+            this.Controls.Add(this.but_Gestionar_Usuarios);
+            this.Controls.Add(this.but_Gestionar_Rubro);
+            this.Controls.Add(this.but_Gestionar_Rol);
+            this.Name = "frm_Tareas_Administrador";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button but_Gestionar_Rol;
+        private System.Windows.Forms.Button but_Gestionar_Rubro;
+        private System.Windows.Forms.Button but_Gestionar_Usuarios;
+        private System.Windows.Forms.Button but_Gestionar_Visibilidad;
+    }
+}
